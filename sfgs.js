e0c6e68945fb645cc64293a24a4282e04ec4b81f
abdelrahman Mohamed <abdelrahman53502@gmail.com>
2026-08-22 18:51:42 +0300

    remove conflicting console.log statements and clean up code

--- sfgs.js
+++ sfgs.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 console.log("hello sfgs sfsfsfs");
 console.log("write line");
 console.log("new from fast")
-=======
-console.log("hello sfgs sfsf");
-console.log("new from three1")
->>>>>>> three
